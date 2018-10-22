36d4c04a6400950b9f18cebefac38d6446397182
Joe Bennett <joe@assimtech.com>
2018-10-22 16:56:32 +1000

    Added sf4 compatible service aliases

--- spec/DependencyInjection/AssimtechFiatExtensionSpec.php
+++ spec/DependencyInjection/AssimtechFiatExtensionSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Assimtech\FiatBundle\DependencyInjection;
 
+use Assimtech\Fiat;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -16,6 +17,32 @@ class AssimtechFiatExtensionSpec extends ObjectBehavior
     function it_can_load(ContainerBuilder $container)
     {
         $configs = array();
+
+        $path = realpath(__DIR__.'/../../src/DependencyInjection').'/../Resources/config/services.yml';
+        $container->fileExists($path)->willReturn(true);
+
+        $serviceId = 'assimtech_fiat.accountant';
+        $serviceClass = Fiat\Accountant::class;
+        $container
+            ->setDefinition($serviceId, Argument::which('getClass', $serviceClass))
+            ->shouldBeCalled()
+        ;
+        $container
+            ->setAlias($serviceClass, Argument::which('__toString', $serviceId))
+            ->shouldBeCalled()
+        ;
+
+        $twigServiceId = 'assimtech_fiat.twig.accountant';
+        $twigServiceClass = Fiat\Twig\Extension\Accountant::class;
+        $container
+            ->setDefinition($twigServiceId, Argument::which('getClass', $twigServiceClass))
+            ->shouldBeCalled()
+        ;
+        $container
+            ->setAlias($twigServiceClass, Argument::which('__toString', $twigServiceId))
+            ->shouldBeCalled()
+        ;
+
         $this->load($configs, $container);
     }
 }
